027c2bf850df2915d90d6c4b491eb297458798e0
Sorany <soherreras@unal.edu.co>
2025-11-22 11:32:26 -0500

    modificaciones

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -11,7 +11,8 @@ from ._internals.write_word_counts import write_word_counts
 def main():
     if len(sys.argv) != 3:
         print("Usage: python wordcount.py <input_folder> <output_folder>")
-        sys.exit(1)
+        # sys.exit(1)
+        return
 
     input_folder = sys.argv[1]
     output_folder = sys.argv[2]
